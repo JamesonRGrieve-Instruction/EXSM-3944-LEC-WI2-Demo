19f6e1168bd211e5d8c7788c0d8f4b7d3cb0fc3d
James Grieve <jgrieve@ualberta.ca>
2023-02-28 20:31:39 -0700

    Validation exception with plural inner execptions;

---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Models/ValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace EXSM3944_Demo.Models
+{
+    public class ValidationException : Exception
+    {
+        public List<Exception> InnerExceptions = new List<Exception>();
+        public override string Message => $"There are ${(InnerExceptions.Count > 0?InnerExceptions.Count:"no")} exceptions. Please view the inner exceptions for more information.";
+        public bool IsError => InnerExceptions.Count > 0;
+    }
+}

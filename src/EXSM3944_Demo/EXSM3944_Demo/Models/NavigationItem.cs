6be7b3e9b868dfbee73b50ccb58f00cd7ce3c7cf
James Grieve <jgrieve@ualberta.ca>
2023-02-28 20:14:28 -0700

    Editing menu and automatically generating it with iterative rendering;

---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Models/NavigationItem.cs
@@ -0,0 +1,15 @@
+﻿namespace EXSM3944_Demo.Models
+{
+    public class NavigationItem
+    {
+        public NavigationItem(string name, string controller, string action)
+        {
+            Name = name;
+            Controller = controller;
+            Action = action;
+        }
+        public string Name = "";
+        public string Controller = "";
+        public string Action = "";
+    }
+}

e6a1cae9a04ae8e639b799f6299b0d0479cb58af
James Grieve <jgrieve@ualberta.ca>
2023-03-08 16:48:41 -0700

    Static list of models; Data grid on Index;

---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using EXSM3944_Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EXSM3944_Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> Products = new List<Product>();
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            Products.Add(new Product() { ID = 1, Name = "Sample", Description = "Sample Product" });
+            return View(Products);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace EXSM3944_Demo.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

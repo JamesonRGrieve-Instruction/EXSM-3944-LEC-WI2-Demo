8ff012e92b6b3c9eec4d2d5aadadc6c5b5ac813e
James Grieve <jgrieve@ualberta.ca>
2023-03-14 19:50:14 -0600

    Authentication;

---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using EXSM3944_Demo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EXSM3944_Demo.Controllers
+{
+    [Authorize]
+    public class PersonController : Controller
+    {
+        private static List<Person> People { get; set; } = new List<Person>();
+
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            //if (!User.Identity.IsAuthenticated) return RedirectToRoute(new { controller = "Identity", action = "Account", id="Login" });
+            return View(People.Where(person => person.UserID == User.Identity.Name));
+        }
+
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm]Person person)
+        {
+            try
+            {
+                person.UserID = User.Identity.Name;
+                People.Add(person);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(person);
+            }
+        }
+    }
+}
---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace EXSM3944_Demo.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}

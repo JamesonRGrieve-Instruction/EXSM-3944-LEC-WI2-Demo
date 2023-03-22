88c19e19426060543ceb84a433a1bbd8d2b2850c
James Grieve <jgrieve@ualberta.ca>
2023-03-21 19:46:14 -0600

    Functional views;

---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Controllers/VehicleController.cs
@@ -0,0 +1,96 @@
+﻿using EXSM3944_Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EXSM3944_Demo.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private static List<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+
+        // GET: VehicleController
+        public ActionResult Index()
+        {
+            return View(Vehicles.Where(vehicle => vehicle.UserID == User.Identity.Name));
+        }
+
+        // GET: VehicleController/Details/5
+        public ActionResult Details(string id)
+        {
+            return View(Vehicles.Single(vehicle => vehicle.VIN == id));
+        }
+
+        // GET: VehicleController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm]Vehicle vehicle )
+        {
+            try
+            {
+                vehicle.UserID = User.Identity.Name;
+                Vehicles.Add(vehicle);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(vehicle);
+            }
+        }
+
+        // GET: VehicleController/Edit/5
+        public ActionResult Edit(string id)
+        {
+            return View(Vehicles.Single(vehicle => vehicle.VIN == id));
+        }
+
+        // POST: VehicleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, [FromForm] Vehicle vehicle)
+        {
+            try
+            {
+                Vehicle target = Vehicles.Single(vehicle => vehicle.VIN == id);
+                target.Model = vehicle.Model;
+                target.Manufacturer = vehicle.Manufacturer;
+                target.Colour = vehicle.Colour;
+                target.ModelYear = vehicle.ModelYear;
+                target.PurchaseDate = vehicle.PurchaseDate;
+                target.SaleDate = vehicle.SaleDate;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(vehicle);
+            }
+        }
+
+        // GET: VehicleController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            return View(Vehicles.Single(vehicle => vehicle.VIN == id));
+        }
+
+        // POST: VehicleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string id, [FromForm] Vehicle vehicle)
+        {
+            try
+            {
+                Vehicles.Remove(Vehicles.Single(vehicle => vehicle.VIN == id));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(vehicle);
+            }
+        }
+    }
+}
---
+++ src/EXSM3944_Demo/EXSM3944_Demo/Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EXSM3944_Demo.Models
+{
+    public class Vehicle
+    {
+        [Required]
+        [StringLength(17, MinimumLength = 17)]
+        // TODO: Regular Expression
+        public string VIN { get; set; }
+        public string UserID { get; set; }
+        [Required]
+        [Range(1900, 2050)]
+        public int ModelYear { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        // TODO: Regular Expression
+        public string Manufacturer { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Model { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Colour { get; set; }
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+        public DateTime? SaleDate { get; set; }
+
+    }
+}
